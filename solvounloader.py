7eda74b7c0b24fe55ac33ef0a3e848ea3607a5f0
Egor Egorov <egigoka@gmail.com>
2017-10-26 23:12:48 +0500

    tripleclick bug

--- solvounloader.py
+++ solvounloader.py
@@ -25,6 +25,10 @@ class Arguments:
         lo = False
         if arg in ["lo"]:  # накладные, привя
             lo = True
+        
+        bugged_tripleclick = False
+        if arg == "btk":
+            bugged_tripleclick = True
 
 
 class State:
@@ -289,8 +293,8 @@ try:
                     sleep(State.before_ctrl_a_sleep)
                     hotkey('ctrl', 'a')                                                             # выделить всё
                     sleep(State.ctrl_a_sleep)                                                       # подождать, пока всё выделится
-                    hotkey('ctrl', 'a')                                                             
-                    sleep(State.ctrl_a_sleep)                                                       
+                    hotkey('ctrl', 'a')
+                    sleep(State.ctrl_a_sleep)
                     dropdown = None                                                                 # меню не выпало
                     while not dropdown:                                                             # пока не выпадет меню:
                         Click.right(move(workarea))                                                     # нажать правой кнопкой по рабочей области
@@ -305,18 +309,18 @@ try:
 
     if Arguments.batch_unload:    # рейсы
         def main():
-            
+
             def unload():
                 Click.right()
                 move(wait_locate("команды...белая", every=0.1, timeout=10))
                 move(wait_locate("отгрузитьбелая", every=0.1, timeout=30))
                 Click.left()
-            
+
             def unload_last():
                 move(locate("готовкотгрузкевыделеннаяw7", "готовкотгрузкевыделеннаяw10"))
                 unload()
-                
-            
+
+
             Open.solvo()
             Open.Solvo.Menu.Documents.shipments()
             Click.left(move(wait_locate("светлозел", every=0.1, timeout=30)))
@@ -336,7 +340,7 @@ try:
                                 position_of_button = wait_locate("buttonup_working", every=0.1, timeout=30)
                             except IndexError:
                                 unload_last()
-                                
+
                             for i in Int.from_to(1,5):
                                 sleep(0.1)
                                 Click.left(move(position_of_button))
@@ -400,4 +404,7 @@ except KeyboardInterrupt:
 
 
 tripleclick.main = main
-tripleclick.start()
+if State.bugged_tripleclick:
+    main()
+else:
+    tripleclick.start()
