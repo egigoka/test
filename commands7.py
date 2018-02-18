5a4ac43a8bc922157d4f6c780d51d1ba013fbe60
Egor Egorov <egigoka@gmail.com>
2018-02-18 16:49:05 +0300

    c7 update Learning.awful_poetry_page65

--- commands7.py
+++ commands7.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import datetime
 start_bench_no_bench = datetime.datetime.now()
-__version__ = "7.26.0-alpha"
+__version__ = "7.27.0-alpha"
 import os
 import sys
 
@@ -1049,6 +1049,37 @@ class Int:
 
 
 class Learning():
+
+    @staticmethod
+    def bubblesort(list, quiet=True):
+        is_sorted = False
+        if not quiet:
+            maxcnt = 0
+            lenlist = len(list)
+        while not is_sorted:
+            cnt = 0
+            while True:
+                try:
+                    if list[cnt] > list[cnt+1]:
+                        temp_var = list[cnt]
+                        list[cnt] = list[cnt+1]
+                        list[cnt+1] = temp_var
+                        if not quiet:
+                            if cnt > maxcnt:
+                                maxcnt = cnt
+                                output_string = Str.leftpad(str(maxcnt), len(str(lenlist)))+"/"+str(lenlist)
+                                if OS.name == "macos":
+                                    output_string = " " + output_string
+                                Print.rewrite(output_string)
+                        break
+                    cnt += 1
+                except IndexError:  # значит, прошлись по всему списку и всё ок
+                    is_sorted = True
+                    break
+        return list
+
+
+
     @staticmethod
     def bigdigits(digits):
         def digits_init(height = False):
@@ -1139,8 +1170,9 @@ class Learning():
                 line = line + Digits[digit][column].replace("#", str(digit)) + " "
             print(line)
             column += 1
+
     @staticmethod
-    def page65():
+    def simple_calc_page65():
         list = []
         try:
             while True:
@@ -1153,13 +1185,30 @@ class Learning():
             print("numbers:", list)
             print("count =", len(list), "lowest =", min(list), "highest =", max(list), "mean =", mean)
 
-
     @staticmethod
-    def simplecalctest():
-        pass
+    def awful_poetry_page65(sentences=5):
+        articles = ["a", "the"]
+        pronouns = ['my', 'your', 'his', 'her', 'its', 'our', 'your', 'their'] + articles
+        pronouns_plural = ['mine', 'yours', 'his', 'hers', 'its', 'ours', 'its', 'ours', 'yours', 'theirs'] + articles
+        pronouns = [pronouns, pronouns_plural]
+        nouns = ["cat", "women", "men", "dog", "cluster", "Sonic the Hedgehog", "queen", "breast"]
+        nouns_multiple = ["cats", "women", "men", "dogs", "clusters", "Sonics the Hedgehogs", "queens", "breasts"]
+        nouns = [nouns, nouns_multiple]
+        verbs = ["jumped", "fucked", "fucks", "sang", "ran", "clusteryfied", "died"]
+        adverbs = ["as fuck", "loudly", "well", "badly", "quetly"]
+
+        for _ in Int.from_to(1,sentences):
+            multiple = Random.integer(0,1)
+            print(pronouns[multiple][Random.integer(0, len(pronouns[multiple])-1)].capitalize(), end=" ")
+            print(nouns[multiple][Random.integer(0, len(nouns[multiple]) - 1)], end=" ")
+            print(verbs[Random.integer(0, len(verbs)-1)], end=" ")
+            print(adverbs[Random.integer(0, len(adverbs) - 1)], end="")
+            print(".", end=" ")
+        print()
 
 
 class Repl:
+    @staticmethod
     def loop(safe=False):
         def main():
             while True:
