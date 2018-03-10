368e50c59d52f54fc28a2d7329f97a05bdd966e9
Egor Egorov <egigoka@gmail.com>
2018-03-10 17:14:13 +0300

    syntax fix

--- commands8.py
+++ commands8.py
@@ -1113,7 +1113,7 @@ class Wget:
                     '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) ' + \
                     'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3167.0 Safari/537.36"'
         if quiet:
-            command = "wget '" + url + f"' -O " + output + " " + arguments
+            command = "wget '" + url + "' -O " + output + " " + arguments
             return Console.get_output(command)
         else:
             url = url.replace("&", backslash + "&")
