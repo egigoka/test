fa5e98823323ac468d68c68cd8cdb3912ea8bbf4
Egor Egorov <egigoka@gmail.com>
2018-03-27 20:15:37 +0300

    sctript clean_clipboard_from_formatting now not cleaning clipboard with non-strings

--- scripts/clean_clipboard_from_formatting.py
+++ scripts/clean_clipboard_from_formatting.py
@@ -14,26 +14,14 @@ oldinputstr = ""
 while True:
     time.sleep(0.1)
     inputstr = copypaste.paste()
+    if inputstr == "":
+        continue
     if oldinputstr != inputstr:
         print (repr(inputstr), newline)
         oldinputstr = inputstr
-    #Print.debug("inputstr", inputstr, "type(inputstr)", type(inputstr), "repr(inputstr)", repr(inputstr))
-    #print(repr(inputstr))
     inputstr = str(inputstr)
     inputstr = inputstr.replace(newline2, "")
     inputstr = inputstr.replace(newline, "")
     inputstr = inputstr.replace("\t", "")
     outputstr = inputstr
-    #for symbol in inputstr:
-    #    if (symbol == newline) or (symbol == newline2):
-    #        continue
-    #    try:
-    #        Print.rewrite(symbol)
-    #        copypaste.copy(symbol)
-    #        outputstr += symbol
-    #    except:
-    #        pass
-    
-    #Print.debug("outputstr", outputstr, "repr(outputstr)", repr(outputstr))
-    #print(repr(outputstr))
-    copypaste.copy(outputstr)
+    #copypaste.copy(outputstr)
