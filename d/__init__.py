bb00d66af4bbc85466814c277436fef35cc7acf5
egigoka <egigoka@gmail.com>
2018-08-21 15:17:44 +0300

    "cleanup"

--- d/__init__.py
+++
@@ -1,5 +0,0 @@
-#! python3
-# -*- coding: utf-8 -*-
-
-import d.a as a
-import d.b as b
--- d/b.py
+++
@@ -1,7 +0,0 @@
-#! python3
-# -*- coding: utf-8 -*-
-
-def b():
-    from d.a import a
-    a()
-    print("called b")
