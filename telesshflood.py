34358fbbeb5c0150db83b6fe93bb8a86aa329536
Egor Egorov <egigoka@gmail.com>
2019-08-09 13:28:58 +0300

    updated telesshflood

--- telesshflood.py
+++ telesshflood.py
@@ -4,11 +4,12 @@ from commands import *
 __version__ = "0.0.2"
 
 servers = ['eggg-2012', 'eggg-2012-na2', 'eggg-2019-na3', 'eggg-2008na4',
-           'eggg-dc1']
+           'eggg-dc1', 'spb-qc203v']
 
 
 def main():
     Time.sleep(5)
+    pyautogui.typewrite(f't 20' + newline)
     for s in servers:
         pyautogui.typewrite(f'ping {s}' + newline)
         Time.sleep(0.1)
