3ebeb260c514197f68e76d31988cc6254947d5ac
Egor Egorov <egigoka@gmail.com>
2019-08-09 20:32:33 +0300

    updated telesshflood - moved sleep

--- telesshflood.py
+++ telesshflood.py
@@ -1,14 +1,13 @@
 import pyautogui
 from commands import *
 
-__version__ = "0.0.2"
+__version__ = "0.0.4"
 
 servers = ['eggg-2012', 'eggg-2012-na2', 'eggg-2019-na3', 'eggg-2008na4',
            'eggg-dc1', 'spb-qc203v']
 
 
 def main():
-    Time.sleep(5)
     pyautogui.typewrite(f't 20' + newline)
     for s in servers:
         pyautogui.typewrite(f'ping {s}' + newline)
@@ -18,5 +17,6 @@ def main():
 
 
 if __name__ == "__main__":
+    Time.sleep(5)
     for range(3):
         main()
