d828f7944f6a7abda26d5fced4892aac1b57eba5
Egor Egorov <egigoka@gmail.com>
2022-04-08 20:13:37 +0500

    added output file to tree_with_sha.py

--- tree_with_sha.py
+++ tree_with_sha.py
@@ -2,8 +2,21 @@ from commands import *
 
 path = OS.args[1]
 
+try:
+    output = OS.args[2]
+except:
+    output = None
+
 for root, dirs, files in OS.walk(path):
     for file in files:
         filepath = Path.combine(root, file)
         sha = File.sha256_checksum(filepath)
         print(filepath, sha)
+        if output is not None:
+            File.write(output, f"{filepath} {sha}{newline}", mode="a")
+
+if output is not None:
+    content = File.read(output)
+    lines = Str.nl(content)
+    lines.sort()
+    File.write(output, newline.join(lines), mode = "w")
