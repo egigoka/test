f128d757f86c7361384bc312619b76148eb920b9
Egorov Egor <egigoka@gmail.com>
2022-11-22 12:49:55 +0600

    updated searcher

--- searcher.py
+++ searcher.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from commands import *
-print("searcher 0.2.6")
+print("searcher 0.4.5")
 
 whoami = Console.get_output("whoami").strip()
 if OS.windows:
@@ -9,55 +9,135 @@ if OS.windows:
 else:
     username = whoami
 
-paths=['.']
-skipped_paths=['']
-file_extensions=[".py"]
-skipped_file_extensions=['.w2p', '.png', '.gif', '.jpg', '.gz', '.ico', '.pdf', '.jpeg', '.icns', '.sqlite', '.pyc']
-match_strings=['height']
-case_sensitive=True
+folder = OS.args[1]
+cache_create = "--cache-create" in OS.args
+cache_load = "--cache-load" in OS.args
+
+paths=['.', folder]
+skipped_paths=['/mnt/c/Windows/',
+               '/mnt/c/program files/',
+               '/mnt/c/program files (x86)/',
+               '/mnt/c/programdata/',
+               '/mnt/c/MSOCache',
+               r'c:\Windows',
+               r'c:\program files',
+               r'c:\program files (x86)',
+               r'c:\programdata',
+               r'c:\MSOCache']
+file_extensions=[".xml"]
+match_strings=['<lol>']
+skipped_strings = []
+case_sensitive=False
 multiple_lines=True
 stop_after_every_found_line=False
 end_print_files_dict=True
 
 _printed_results = []
 
-
-def print_result(file, line, line_cnt):
+def print_result(file, line, line_cnt, found_string):
     Print.rewrite()
     if file not in _printed_results:
         Print.colored(file, "green")
     if file not in _printed_results or multiple_lines:
-        Print.colored(fr"{line_cnt}:{line}", "yellow")
+        line = line.replace(found_string, Print.colored(found_string, "red", verbose=False))
+        Print.colored(fr"{line_cnt}:{line}", "magenta")
         _printed_results.append(file)
+
+
+def check_strings(string, substring, case_sensitive):
+    if case_sensitive:
+        string_needed_case = string
+        substring_needed_case = substring
+    elif not case_sensitive:
+        string_needed_case = string.lower()
+        substring_needed_case = substring.lower()
+    
+    return substring in string
 
 
 files_to_read = []
 
-print(f"{Time.dotted()} search files")
-for path in paths:
-    path = Path.combine(path, "")
-    for root, dirs, files in os.walk(path):
-        skipped = False
-        for skipped_path in skipped_paths:
-            if root.lower().startswith(Path.combine(skipped_path, "").lower()) and skipped_path != "":
-                skipped = True
-                break
-        if not skipped:
-            for file in files:
-                file_path = Path.combine(root, file)
-                for ext in file_extensions:
-                    if File.get_extension(file) == ext:
-                        files_to_read.append(file_path)
+if cache_create or cache_load:
+    files_to_read = JsonList("searcher_cache.json")
+if cache_create:
+    files_to_read.string = []
 
-print(f"{Time.dotted()} read {len(files_to_read)} files")
+print(f"{Time.dotted()} search files")
+
+cnt = 0
+if not cache_load:
+    for path in paths:
+        path = Path.combine(path, "")
+        for root, dirs, files in os.walk(path):
+            cnt += 1
+            if cnt % 100 == 0:
+                Print.rewrite(f"{cnt}/146431", root)
+            skipped = False
+            for skipped_path in skipped_paths:
+                
+                if root.lower().startswith(skipped_path.lower()) and skipped_path != "":
+                    skipped = True
+                    break
+            if not skipped:
+                for file in files:
+                    file_path = Path.combine(root, file)
+                    for ext in file_extensions:
+                        if File.get_extension(file).lower() == ext.lower():
+                            files_to_read.append(file_path)
+
+if cache_create:
+    files_to_read.save()
+
+len_files_to_read = len(files_to_read)
+
+Print.rewrite()
+print(f"{Time.dotted()} {len_files_to_read} files to read from {cnt} folders")
+cnt = 0
+files = []
+b = Bench()
 for file in files_to_read:
-    for line_cnt, line in enumerate(Str.nl(File.read(file))):
+    time = b.end(start_immediately=True)
+    if time > 1:
+        print(time, b.prefix)
+    cnt += 1
+    if cnt % 100 == 0:
+        Print.rewrite(f"{cnt}/{len_files_to_read}", file)
+    try:
+        file_content = File.read(file)
+    except (PermissionError, FileNotFoundError) as e:
+        Print.rewrite()
+        print(e)
+        continue
+    file_lines = Str.nl(file_content)
+    for line_cnt, line in enumerate(file_lines):
+        skip = False
+        for string in skipped_strings:
+            if check_strings(line, string, case_sensitive):
+                skip = True
+                break
+        if skip:
+            break
+    if skip:
+            continue
+    add_to_result = False
+    for line_cnt, line in enumerate(file_lines):
         for string in match_strings:
-            if case_sensitive:
-                if string in line:
-                    print_result(file, line, line_cnt)
-            else:
-                if string.lower() in line.lower():
-                    print_result(file, line, line_cnt)
+            if check_strings(line, string, case_sensitive):
+                print_result(file, line, line_cnt, string)
+                add_to_result = True
+    if add_to_result:
+        files.append(file)
+    b.prefix = file
+
+time = b.end(start_immediately=True)
+if time > 1:
+    print(time, b.prefix)
 
 print(f"{Time.dotted()} end")
+
+print()
+
+if end_print_files_dict:
+    for file in files:
+        print('npp "' + file + '"')
+    
