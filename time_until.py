2291871450abe77957576e8a26f7060ef48f0dd1
Egorov Egor <egigoka@gmail.com>
2023-05-04 16:47:54 +0600

    updated time_until to make text more condenced

--- time_until.py
+++ time_until.py
@@ -28,25 +28,48 @@ for end in ends:
 	
 	new_endtime = Time.datetime(hour = end[0], minute = end[1], second = 0)
 
-	if new_endtime < last_endtime:
-		new_endtime = new_endtime.replace(day=last_endtime.day + 1)
+	while True:
+		if new_endtime < last_endtime:
+			new_endtime = new_endtime + timedelta(days=1)
+		else:
+			break
 	
 	endtimes.append(new_endtime)
 
 	last_endtime = new_endtime
 
+#debug
+# for endtime in endtimes:
+	# print(endtime)
+#debug END
+
 endtimes.sort()
+
+#debug
+# print()
+# for endtime in endtimes:
+	# print(endtime)
+#debug END
 
 # cnt = Json("time_until_cnt.json")
 # if not isinstance(cnt.string, int):
 	# cnt.string = 0
 
-for endtime in endtimes:
-	while True:
-	
-		Print.rewrite()
-		seconds = (endtime-now()).seconds
+while True:
+	Console.clean()
+	for endtime in endtimes:
+		time = now()
+
+		# Print.debug(f"{endtime=}")
+		# Print.debug(f"{time=}")
+		
+		if endtime < time:
+			continue
+		
+		seconds = int((endtime-time).total_seconds())
+		# Print.debug(f"{seconds=}")
 		human_readable = Time.human_readable(seconds)
+		# Print.debug(f"{human_readable=}")
 
 		# font = Random.item(fonts)
 		# try:
@@ -54,24 +77,27 @@ for endtime in endtimes:
 		# except IndexError:
 			# cnt.string = 0
 			# font = fonts[cnt.string]
-		font = "minecraft"
-
+		# font = "minecraft"
+		font = "minecraft_condenced"
+		
 		# cnt.string += 1
 		# cnt.save()
 		
 		# print(fterm.renderText(f"{font} {cnt.string}/{len(fonts)}"))
 		# print(fterm.renderText(f"{font}"))
 
-		Console.clean()
-		
 		f = Figlet(font=font, justify="center", width=Console.width())
+
+		until = f"{endtime.hour}:{endtime.minute}"
 		
-		print(f.renderText(human_readable))
-		print(f.renderText(f"until {endtime.hour}:{endtime.minute}"))
+		if endtime.day != time.day:
+			until = f"{endtime.day:02}.{endtime.month:02} {until}"
+		
+		print(f.renderText(f"{human_readable} until {until}").rstrip())
 
-		if seconds <= 0:
-			Console.blink()
-			break
-
-		Time.sleep(1)
+		# if seconds <= 0:
+			# Console.blink()
+			# break
+	# OS.exit(1)
+	Time.sleep(1)
 	
