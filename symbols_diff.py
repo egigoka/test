24527e6779637fd32e14f5d1a21cde2beaf7b18b
Egorov Egor <egigoka@gmail.com>
2023-05-10 12:23:20 +0600

    added sleep function to symbols_diff

--- symbols_diff.py
+++ symbols_diff.py
@@ -5,6 +5,14 @@ qq = q
 for sym in "qwertyuiopasdfghjklzxcvbnm":
 	exec(f"{sym} = ord('{sym}')")
 
+
+def sleep(minutes):
+	try:
+		Time.sleep(60*minutes, verbose=True)
+	except KeyboardInterrupt:
+		Print.rewrite()
+
+
 def diff(sym, symb):
 	number = int((sym+symb) / 2)
 	char = chr(number)
