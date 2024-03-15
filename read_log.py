688e47204cfff3a2b6d494c576fe0b844ae657cf
Egorov Egor <egigoka@gmail.com>
2024-03-15 15:44:30 +0500

    refactor read_log

--- read_log.py
+++ read_log.py
@@ -6,29 +6,24 @@ import math
 from datetime import datetime
 from commands import Time, Console, Print, ID
 from smbprotocol.connection import Connection, Dialects
-from smbprotocol.open import Open, CreateDisposition, FilePipePrinterAccessMask, FileAttributes, ImpersonationLevel, ShareAccess, CreateOptions
+from smbprotocol.open import (Open, CreateDisposition, FilePipePrinterAccessMask, FileAttributes, ImpersonationLevel,
+                              ShareAccess, CreateOptions)
 from smbprotocol.session import Session
 from smbprotocol.tree import TreeConnect
 from smbprotocol.exceptions import SMBResponseException, SharingViolation, SMBConnectionClosed, SMBException
 
-csv.field_size_limit(sys.maxsize)
-line_cnt = ID()
-
 
 def clear_cache():
-    global cache
-    cache = {max(cache.keys()): cache[max(cache.keys())]}
+    global CACHE
+    CACHE = {max(CACHE.keys()): CACHE[max(CACHE.keys())]}
 
 
-def decode_utf8(bytearray):
+def decode_utf8(byte_array):
     try:
-        return bytearray[::-1].decode('utf-8', errors='replace')
+        return byte_array[::-1].decode('utf-8', errors='replace')
     except UnicodeError:
-        print(bytearray[::-1])
+        print(byte_array[::-1])
         raise
-
-
-cache = {}
 
 
 def smb_read_with_retry(file, position, length):
@@ -36,91 +31,81 @@ def smb_read_with_retry(file, position, length):
         try:
             return file.read(position, length)
         except SMBResponseException:
-            Time.sleep(1, verbose = True)
+            Time.sleep(1, verbose=True)
 
 
-def get_byte_from_cache(bytes, position, file, cached_position, length):
-    cached = bytes[position:position+1]
+def get_byte_from_cache(byte_array, position, file, cached_position, length):
+    global CACHE
+    cached = byte_array[position:position + 1]
     if cached:
         return cached
     else:
         new_cache = smb_read_with_retry(file, cached_position, length)
-        cache[cached_position] = new_cache
+        CACHE[cached_position] = new_cache
         return new_cache[position:position+1]
 
 
-def read_cached(file, position, cache_length = 4096):
+def read_cached(file, position, cache_length=4096):
+    global CACHE
     if cache_length == 1:
         return file.read(position, 1)
     
     cached_position = position - position % cache_length
-    if not cached_position in cache:
-        cache[cached_position] = smb_read_with_retry(file, cached_position, cache_length)
-    out = get_byte_from_cache(cache[cached_position], position - cached_position, file, cached_position, cache_length)
+    if cached_position not in CACHE:
+        CACHE[cached_position] = smb_read_with_retry(file, cached_position, cache_length)
+    out = get_byte_from_cache(CACHE[cached_position], position - cached_position, file, cached_position, cache_length)
     return out
 
-file_path = "Public\\Logs\\МуравьинаяЛогистика.csv"
 
-# Set up the SMB connection
-connection = Connection(uuid.uuid4(), server, 445)
-connection.connect(Dialects.SMB_3_0_2)
+def restart_connection():
+    # Set up the SMB connection
+    connection = Connection(uuid.uuid4(), server, 445)
+    connection.connect(Dialects.SMB_3_0_2)
 
-# Start an SMB session
-session = Session(connection, user, password)
-session.connect()
+    # Start an SMB session
+    session = Session(connection, user, password)
+    session.connect()
 
-# Connect to the SMB share
-tree = TreeConnect(session, "\\\\{}\\{}".format(server, share))
-tree.connect()
+    # Connect to the SMB share
+    tree = TreeConnect(session, "\\\\{}\\{}".format(server, share))
+    tree.connect()
 
-# init logic
-newline = "\n"
-bnewline = b'\n'
+    return tree
 
-num_lines = int(sys.argv[1])
 
-first_run = True
+def open_file_smb(tree, file_path):
+    open_file = Open(tree, file_path)
+    open_file.create(
+        ImpersonationLevel.Impersonation,
+        FilePipePrinterAccessMask.FILE_READ_DATA | FilePipePrinterAccessMask.FILE_READ_ATTRIBUTES,
+        FileAttributes.FILE_ATTRIBUTE_NORMAL,
+        ShareAccess.FILE_SHARE_READ,
+        CreateDisposition.FILE_OPEN,
+        CreateOptions.FILE_NON_DIRECTORY_FILE)
 
-last_position = 0
-
-buffer = bytearray()
-
-def open_file_safely(tree, file_path):
-    while True:
-        try:
-            open_file = Open(tree, file_path)
-            open_file.create(
-                ImpersonationLevel.Impersonation,
-                FilePipePrinterAccessMask.FILE_READ_DATA | FilePipePrinterAccessMask.FILE_READ_ATTRIBUTES,
-                FileAttributes.FILE_ATTRIBUTE_NORMAL,
-                ShareAccess.FILE_SHARE_READ,
-                CreateDisposition.FILE_OPEN,
-                CreateOptions.FILE_NON_DIRECTORY_FILE
-            )
-            break
-        except (SharingViolation, SMBConnectionClosed, SMBException) as e:
-            Time.sleep(1, verbose = True)
-            continue
     return open_file
 
-while True:
-    # init    
-    lines = []
+
+def open_file_safely(file_path):
+    global TREE
+    if not TREE:
+        TREE = restart_connection()
 
     # open the file
-    open_file = open_file_safely(tree, file_path)
+    try:
+        open_file = open_file_smb(TREE, file_path)
+    except (SMBResponseException, SharingViolation, SMBConnectionClosed, SMBException):
+        TREE = restart_connection()
+        open_file = open_file_smb(TREE, file_path)
 
-    # Query the file size
-    position = open_file.end_of_file - 1
+    return open_file
 
-    # save the position
-    position_end = position
-    previous_line = 0
-    if first_run:
-        last_position = position
-    
-    # read the file
-    # print(f"{len(lines)} < {num_lines} and {first_run} or {position} > {last_position}")
+
+def read_file(open_file, position, last_position, first_run, num_lines, previous_line):
+    lines = []
+    bytes_newline = b'\n'
+    buffer = bytearray()
+
     cnt_lines = 0
     cnt_chars = 0
     while (len(lines) < num_lines and first_run) or position > last_position:
@@ -137,9 +122,9 @@ while True:
         position -= 1
 
         # append the character to the buffer
-        if character == bnewline:
+        if character == bytes_newline:
             lines.append(decode_utf8(buffer))
-            
+
             buffer = bytearray()
             cnt_lines += 1
         else:
@@ -148,90 +133,176 @@ while True:
     # append the last line
     if buffer:
         lines.append(decode_utf8(buffer))
-        buffer = bytearray()
-    
-    # update the last position
-    last_position = position_end
 
-    # close the file
-    open_file.close()
+    return lines
 
-    # print the lines
-    for line in lines[::-1]:
-        if line.strip() == "":
-            continue
-        color = None
-        cnt_line = line_cnt.get()
-        if line.strip().endswith('"Егоров Егор"'):
-            color = "magenta"
-            back = "on_white"
-        elif line.strip().endswith('"Мулинов Ерик"'):
-            color = "blue"
-            back = "on_white"
-        else:
-            color = "black"
-            back = "on_white"
 
-        if cnt_line % 2 == 1:
-            back = ""
-        reader = csv.reader([line], delimiter=';', quotechar='"')
-        row = reader.__next__()
-        line = "| "
-        widths = [19, 46, 79, 5, 999, 999, 42]
-        sum_widths = sum(widths) + 20
-        console_width = Console.width()
-        if sum_widths > console_width:
-            widths[4] -= math.ceil((sum_widths - console_width) / 2)
-            widths[5] -= math.ceil((sum_widths - console_width) / 2)
-        lines_current = []
-        for cnt in reversed(range(10)):
-            sublines = []
-            is_any = False
-            for i in range(len(row)):
-                try:
-                    width = widths[i]
-                except IndexError:
+def get_colors(row, line_print_number):
+    level = row[3]
+    username = row[6]
+
+    if username.strip() == "Егоров Егор":
+        color = "magenta"
+        back = "on_white"
+    elif username.strip() == "Мулинов Ерик":
+        color = "blue"
+        back = "on_white"
+    else:
+        color = "black"
+        back = "on_white"
+
+    if line_print_number % 2 == 1:
+        back = ""
+
+    if level in ["Warning"]:
+        color = "yellow"
+    elif level in ["Error"]:
+        color = "red"
+    elif level in ["Fatal", "Unknown"]:
+        color = "white"
+        back = "on_red"
+    elif level in ["Debug", "Info"]:
+        color = "white"
+        back = "on_blue"
+
+    return color, back
+
+
+def csv_line_to_list(string):
+    reader = csv.reader([string], delimiter=';', quotechar='"')
+    row = reader.__next__()
+    return row
+
+
+def get_widths():
+    widths = [19, 46, 79, 5, 999, 999, 42]
+    sum_widths = sum(widths) + 20
+    console_width = Console.width()
+    if sum_widths > console_width:
+        widths[4] -= math.ceil((sum_widths - console_width) / 2)
+        widths[5] -= math.ceil((sum_widths - console_width) / 2)
+    return widths
+
+
+def get_time(row):
+    time = row[0]
+    time = time.replace("\ufeff", "")
+    time = time.replace('"', "")
+    try:
+        time = datetime.strptime(time, "%d.%m.%Y %H:%M:%S")
+    except ValueError:
+        Print.debug(time)
+        raise
+    return time
+
+
+def format_substring(substring, row_cnt, width):
+    is_any = False
+    substring = f"{substring}".replace("\t", "\\t")
+
+    start = width * (9 - row_cnt)
+    end = start + width
+
+    if start < 0 and end < 0:
+        start = 0
+        end = 0
+    if end < 0:
+        end = len(substring)
+    if start < 0:
+        start = 0
+
+    substring = substring[start:end]
+
+    if substring:
+        is_any = True
+    substring = f"{substring.ljust(width)} |"
+    if row_cnt == 0 and len(substring.strip()) != 1:
+        substring = substring[:-4] + ">>>|"
+    return substring, is_any
+
+
+def get_substrings(row, row_cnt, widths):
+    substrings = []
+    is_any = False
+    for i, subline in enumerate(row):
+        try:
+            width = widths[i]
+        except IndexError:
+            break
+        substring, is_any = format_substring(subline, row_cnt, width)
+        substrings.append(substring)
+    return substrings, is_any
+
+
+def main():
+    # init logic
+    csv.field_size_limit(sys.maxsize)
+    line_cnt = ID()
+    last_time = datetime.now()
+    first_run = True
+    num_lines = int(sys.argv[1])
+    last_position = 0
+    file_path = "Public\\Logs\\МуравьинаяЛогистика.csv"
+
+    while True:
+        open_file = open_file_safely(file_path)
+
+        # get the file size
+        position = open_file.end_of_file - 1
+
+        # save the position
+        position_end = position
+        previous_line = 0
+        if first_run:
+            last_position = position
+
+        # read the file
+        lines_from_file = read_file(open_file, position, last_position, first_run, num_lines, previous_line)
+        open_file.close()
+
+        # update the position
+        last_position = position_end
+
+        # print the lines
+        for cnt_line, line in enumerate(lines_from_file[::-1]):
+            if line.strip() == "":
+                continue
+
+            line_print_number = line_cnt.get()
+            row = csv_line_to_list(line)
+            color, back = get_colors(row, line_print_number)
+            widths = get_widths()
+            last_time = get_time(row)
+            lines_to_print = []
+
+            for cnt in reversed(range(10)):
+                substrings, is_any = get_substrings(row, cnt, widths)
+
+                if is_any:
+                    lines_to_print.append(" ".join(substrings))
+                else:
                     break
-                subline = f"{row[i]}".replace("\t", "\\t")
 
-                start = width * (9-cnt)
-                end = start + width
+            for line_current in lines_to_print:
+                if back:
+                    Print.colored(line_current, back, color)
+                else:
+                    Print.colored(line_current, color)
 
-                if start < 0 and end < 0:
-                    start = 0
-                    end = 0
-                if end < 0:
-                    end = len(subline)
-                if start < 0:
-                    start = 0
-                
-                # print(f"{start} {end} {subline}")
-                subline = subline[start:end]
+        # print current time and time since last log
+        now = datetime.now()
+        diff = now - last_time
+        diff_formatted = f"{diff.seconds // 3600:02}:{(diff.seconds // 60) % 60:02}:{diff.seconds % 60:02}"
+        Print.colored(f'{now.strftime("%d.%m.%Y %H:%M:%S")} ({diff_formatted} since last log)', "green", end="\r")
 
-                # print(f"{start} {end} {subline}")
-                # print()
-                if subline:
-                    is_any = True
-                subline = f"{subline.ljust(width)} |"
-                width = width + 2
-                if cnt == 0 and len(subline.strip()) != 1:
-                    subline = subline[:-4] + ">>>|"
-                sublines.append(subline)
-            # print(f"{cnt} {sublines} {is_any=}")
-            if is_any:
-                lines_current.append(" ".join(sublines))
-            else:
-                break
-        for line_current in lines_current:
-            if back:
-                Print.colored(line_current, back, color)
-            else:
-                Print.colored(line_current, color)
-    Print.colored(datetime.now().strftime("%d.%m.%Y %H:%M:%S"), "green", end = "\r")
+        clear_cache()
+        first_run = False
 
-    clear_cache()
-    first_run = False
-    
-    Time.sleep(1, verbose = False)
+        Time.sleep(1, verbose=False)
 
 
+CACHE = {}
+TREE = None
+
+if __name__ == "__main__":
+    main()
