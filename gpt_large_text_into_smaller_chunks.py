dd802df333006ae3f4a5282a03a05c6800ab03a5
Egorov Egor <egigoka@gmail.com>
2024-05-24 16:49:24 +0500

    rewrite some logic in gpt_large_text_into_smaller_chunks, enhance prompt for gpt3.5

--- gpt_large_text_into_smaller_chunks.py
+++ gpt_large_text_into_smaller_chunks.py
@@ -1,4 +1,4 @@
-from commands import *
+from commands import OS, File, List, copy, paste
 
 input_file = OS.args[1]
 
@@ -6,22 +6,43 @@ content = File.read(input_file)
 
 output_file = input_file + ".smaller.txt"
 
-chunks = List.split_every(content, int(4000*3.75-100))
+CONTEXT_WINDOW = 4096
+
+PROMPT_LENGTH_TOKENS = 200
+
+TOKEN_MULTIPLIER_UNICODE = 1.5
+TOKEN_MULTIPLIER_EN = 3.75
+
+TOKEN_MULTIPLIER = TOKEN_MULTIPLIER_EN
+if "u" in OS.args:
+    TOKEN_MULTIPLIER = TOKEN_MULTIPLIER_UNICODE
+
+CHUNKS = List.split_every(content, int(CONTEXT_WINDOW*TOKEN_MULTIPLIER-PROMPT_LENGTH_TOKENS))
 
 File.wipe(output_file)
 
-for cnt, chunk in enumerate(chunks):
-    print("chunk copied to clipboard, feed it into chatgpt")
-    copy(f"Please, remove some information not applicable to main topic of text that i'll send you in chunks. "
-         + f"Only save most valuable information that will be helpful in solving problem. "
-         + f"Save related practices and commands that can be helpful in solving problems. "
-         + f"I'ts chunk #\"{cnt}\". "
-         + f"Content of chunk: \"\"\"{chunk}\"\"\"")
+total = len(CHUNKS)
+for cnt, chunk in enumerate(CHUNKS):
+    chunk_cnt = cnt + 1
+    print(f"{chunk_cnt} / {total} chunk request copied to clipboard, feed it into chatgpt")
+    prompt = (f"Content: \"\"\"{chunk}\"\"\"\n"
+            + f"It's chunk of data #{chunk_cnt} of {total} chunks total.\n\n"
+            + "Please remove information that isn't valuable.\n\n"
+            + "Only save the most valuable information that will be helpful.\n\n"
+            + "Write all valuable information from this chunk of text.\n\n"
+            + "Format it as code for easier copying, please.\n\n"
+            + "Use markdown to emphasize information of the most importance. Thank you."
+            )
+    copy(prompt)
 
-    print("press Enter when output is in your clipboard")
+    print("press Enter when answer is in clipboard")
     input()
     output = paste()
 
-    File.write(output_file, output + newline)
+    if prompt.strip() != output.strip():
+        print("saved new info")
+        File.write(output_file, f"<<<{chunk_cnt}>>>\n{output}\n\n", mode="a")
+    else:
+        print("no new info, skipping")
 
 print(f"done, check file {output_file}")
