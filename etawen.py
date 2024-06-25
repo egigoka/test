eeab6b2f492d1ca2f469a83696d1d13f0b484241
egigoka <egigoka@gmail.com>
2024-06-25 18:08:36 +0500

    added more statistics output to etawen

--- etawen.py
+++ etawen.py
@@ -47,10 +47,15 @@ class TaskProgress:
         self.history = deque(maxlen=window_size)
         self.start_time = time.time()
         self.last_time = self.start_time
+        self.prev_time = self.start_time
+
+    def get_diff(self):
+        return self.last_time - self.prev_time
 
     def add_progress(self, percent):
         current_time = time.time()
         self.history.append((current_time, percent))
+        self.prev_time = self.last_time
         self.last_time = current_time
 
     def get_avg_speed(self):
@@ -103,21 +108,29 @@ def str_datetime():
 
 def print_progress(progress_tracker):
     estimated_completion = progress_tracker.estimate_completion()
-    if estimated_completion:
-        estimated_time_left = estimated_completion - time.time()
-        estimated_time_left = int(estimated_time_left)
-        percent_left = ""
-        if progress_tracker.max_percent != 100:
-            percent_left = progress_tracker.max_percent - progress_tracker.history[-1][1]
-            percent_left = percent_left / (progress_tracker.max_percent / 100)
 
-            percent_left = f"({percent_left:.0f}%) "
-
-        output = (f"| Left {percent_left}: {Time.human_readable(estimated_time_left)}"
-                  f" | Completion: {time.ctime(estimated_completion)}")
-    else:
+    if not estimated_completion:
         output = "Not enough data to estimate completion time."
-    print(str_datetime(), output)
+        print(str_datetime(), output, end="")
+        return 
+
+    estimated_time_left = estimated_completion - time.time()
+    estimated_time_left = int(estimated_time_left)
+    percent_left = ""
+    if progress_tracker.max_percent != 100:
+        percent_left = progress_tracker.max_percent - progress_tracker.history[-1][1]
+        percent_left = percent_left / (progress_tracker.max_percent / 100)
+
+        percent_left = f"({percent_left:.0f}%) "
+
+    diff = progress_tracker.get_diff()
+
+    output = (f"| Left {percent_left}: {Time.human_readable(estimated_time_left)}"
+              f" | Completion: {time.ctime(estimated_completion)}"
+              f" | Speed: {progress_tracker.get_avg_speed()*60:.2f} %/m,"
+              f" | Diff: {Time.human_readable(int(diff))}")
+
+    print(str_datetime(), output, end="")
 
 
 def main():
@@ -133,21 +146,26 @@ def main():
         progress_tracker = TaskProgress(max_percent=MAX_PERCENT)  # Example: custom max percent
 
     print_progress(progress_tracker)
+    print()
     while True:
         percent_input = None
         try:
             Print.colored("Enter the percentage of task completed: ", "green", end="", flush=True)
             percent_input = input()
             percent = float(percent_input)
+
             if REVERSED:
                 percent = progress_tracker.max_percent - percent
-            if percent >= progress_tracker.max_percent:
-                print(f"{str_datetime()} | Task completed! {percent} of {progress_tracker.max_percent}")
-                break
 
             progress_tracker.add_progress(percent)
 
             print_progress(progress_tracker)
+
+            if percent >= progress_tracker.max_percent:
+                print(f" | Task completed!")
+                break
+            else:
+                print()
 
             progress_tracker.save(CACHE_PATH)
             Time.sleep(TIMER, verbose=True)
@@ -156,6 +174,11 @@ def main():
             if percent_input.strip() == "q":
                 print("Exiting.")
                 sys.exit(0)
+            elif percent_input.strip() == "r":
+                print("Resetting progress.")
+                progress_tracker = TaskProgress(max_percent=MAX_PERCENT)
+                progress_tracker.save(CACHE_PATH)
+                continue
                 
             print("Please enter a valid percentage.")
         except KeyboardInterrupt:
