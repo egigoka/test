1c373c73fdf730bbb065b9b0e86d9e8fab131b5e
egigoka <egigoka@gmail.com>
2026-06-17 10:41:40 +0500

    fix KerError typo and message typos in ytdlp_check_archive.py

--- ytdlp_check_archive.py
+++ ytdlp_check_archive.py
@@ -12,7 +12,7 @@ llf = {}
 for f in ff:
     id = Str.substring(f[-19:], "[", "]")
     if id in fff.keys():
-        print(f"file with id {id} are appeared twice: {f} and {fff[id]}")
+        print(f"file with id {id} appeared twice: {f} and {fff[id]}")
         raise KeyError
     fff[id] = f
 
@@ -22,18 +22,18 @@ for l in ll:
         continue
     id = Str.substring(l, "youtube ")
     if id in llf.keys():
-        print(f"archive with id {id} are appeared twice: {l} and {llf[id]}")
-        raise KerError
+        print(f"archive with id {id} appeared twice: {l} and {llf[id]}")
+        raise KeyError
     llf[id] = l
 
 cnt = 0
 for id1, f in fff.items():
     if id1 not in llf.keys():
-        print(f"{cnt} file {id1} {f} is an impopstor")
+        print(f"{cnt} file {id1} {f} is an impostor")
     cnt += 1
 
 cnt = 0
 for id2, l in llf.items():
     if id2 not in fff.keys():
-        print(f"{cnt} video {id2} not doewnloaded")
+        print(f"{cnt} video {id2} not downloaded")
     cnt += 1
